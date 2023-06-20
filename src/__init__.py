336c6136d05df4b497adca7d837e474a8373ec1a
Felipe Peña <felipe.aliante94@gmail.com>
2023-06-20 13:04:25 -0400

    Se sube un Diccionario de Api

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from src.controllers.controller import api_producto
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.register_blueprint(api_producto)
+
+@app.route('/', methods=['GET'])
+def get_productos():
+    return render_template("index.html")
